7e06e1832ca2248ad9fc8de44568de923ff5e24b
kshneo0 <kshneo@gmail.com>
2022-11-29 21:12:35 +0900

    Dong Batch - Job, Step, FlatFileItemReader

---
+++ src/main/java/com/fastcampus/housebatch/job/lawd/LawdFieldSetMapper.java
@@ -0,0 +1,28 @@
+package com.fastcampus.housebatch.job.lawd;
+
+import com.fastcampus.housebatch.core.entity.Lawd;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+/**
+ * fileName : LawdFieldSetMapper
+ * author :  KimSangHoon
+ * date : 2022/11/29
+ */
+public class LawdFieldSetMapper implements FieldSetMapper<Lawd> {
+    public static final String LAWD_CD = "lawdCd";
+    public static final String LAWD_DONG = "lawdDong";
+    public static final String EXIST = "exist";
+
+    private static final String EXIST_TRUE = "존재";
+
+    @Override
+    public Lawd mapFieldSet(FieldSet fieldSet) throws BindException {
+        Lawd lawd = new Lawd();
+        lawd.setLawdCd(fieldSet.readString(LAWD_CD));
+        lawd.setLawdDong(fieldSet.readString(LAWD_DONG));
+        lawd.setExist(fieldSet.readBoolean(EXIST, EXIST_TRUE));
+        return lawd;
+    }
+}
