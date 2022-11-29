3d8310a832728a2dd5574208879b5fee50762cb5
kshneo0 <kshneo@gmail.com>
2022-11-29 20:33:40 +0900

    first commit

---
+++ src/main/java/com/fastcampus/housebatch/config/HouseBatchJpaConfig.java
@@ -0,0 +1,14 @@
+package com.fastcampus.housebatch.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * fileName : HouseBatchJpaConfig
+ * author :  KimSangHoon
+ * date : 2022/11/29
+ */
+@Configuration
+@EnableJpaAuditing
+public class HouseBatchJpaConfig {
+}
---
+++ src/main/java/com/fastcampus/housebatch/core/repository/LawdRepository.java
@@ -0,0 +1,12 @@
+package com.fastcampus.housebatch.core.repository;
+
+import com.fastcampus.housebatch.core.entity.Lawd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * fileName : LawdRepository
+ * author :  KimSangHoon
+ * date : 2022/11/29
+ */
+public interface LawdRepository extends JpaRepository<Lawd, Long> {
+}
