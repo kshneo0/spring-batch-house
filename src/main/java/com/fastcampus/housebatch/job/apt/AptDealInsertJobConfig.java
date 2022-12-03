ce78239379a7e6aaefd9b22d58769a010dfacd2b
kshneo0 <kshneo@gmail.com>
2022-12-03 09:39:39 +0900

    Deal Batch - Job, Step Complete

--- src/main/java/com/fastcampus/housebatch/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/com/fastcampus/housebatch/job/apt/AptDealInsertJobConfig.java
@@ -1,6 +1,7 @@
 package com.fastcampus.housebatch.job.apt;
 
 import com.fastcampus.housebatch.core.dto.AptDealDto;
+import com.fastcampus.housebatch.job.validator.FilePathParameterValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -8,14 +9,24 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 /**
  * fileName : AptDealInsertJobConfig
  * author :  KimSangHoon
  * date : 2022/12/03
+ *
+ * --spring.batch.job.names=aptDealInsertJob -filePath=apartment-api-response.xml
+ *
  */
 @Configuration
 @RequiredArgsConstructor
@@ -25,20 +36,55 @@ public class AptDealInsertJobConfig {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job aptDealInsertJob(Step aptDealInsertStep){
+    public Job aptDealInsertJob(Step aptDealInsertStep) {
         return jobBuilderFactory.get("aptDealInsertJob")
                 .incrementer(new RunIdIncrementer())
+                .validator(new FilePathParameterValidator())
                 .start(aptDealInsertStep)
                 .build();
     }
 
     @JobScope
     @Bean
-    public Step aptDealInsertStep(){
+    public Step aptDealInsertStep(StaxEventItemReader<AptDealDto> aptDealResourceReader
+//                                  ,ItemWriter<AptDealDto> aptDealWriter
+    ) {
         return stepBuilderFactory.get("aptDealInsertStep")
-                .<AptDealDto,AptDealDto>chunk(10)
-                .reader(/*XML 리더*/)
-                .writer()
+                .<AptDealDto, AptDealDto>chunk(10)
+                .reader(aptDealResourceReader)
+                .writer(aptDealWriter())
                 .build();
+    }
+
+    @StepScope
+    @Bean
+    public StaxEventItemReader<AptDealDto> aptDealResourceReader(
+            @Value("#{jobParameters['filePath']}") String filePath,
+            Jaxb2Marshaller aptDealDtoMarshaller
+    ) {
+        return new StaxEventItemReaderBuilder<AptDealDto>()
+                .name("aptDealResourceReader")
+                .resource(new ClassPathResource(filePath))
+                .addFragmentRootElements("item")
+                .unmarshaller(aptDealDtoMarshaller)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Jaxb2Marshaller aptDealDtoMarshaller() {
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        jaxb2Marshaller.setClassesToBeBound(AptDealDto.class);
+        return jaxb2Marshaller;
+
+    }
+
+    //    @StepScope
+//    @Bean
+    //public
+    private ItemWriter<AptDealDto> aptDealWriter() {
+        return items -> {
+            items.forEach(System.out::println);
+        };
     }
 }
