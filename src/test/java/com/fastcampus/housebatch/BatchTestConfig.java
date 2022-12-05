8e5d81c0f8821771f8d45ec05c8bf6e9cf8a1f99
kshneo0 <kshneo@gmail.com>
2022-12-05 20:15:00 +0900

    Dong Batch - add Test

---
+++ src/test/java/com/fastcampus/housebatch/BatchTestConfig.java
@@ -0,0 +1,16 @@
+package com.fastcampus.housebatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * fileName : BatchTestConfig
+ * author :  KimSangHoon
+ * date : 2022/12/05
+ */
+@Configuration
+@EnableBatchProcessing
+@EnableAutoConfiguration
+public class BatchTestConfig {
+}
